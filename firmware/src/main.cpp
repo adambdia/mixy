d170ab7e3178dc303985ff5a48094f8975acc653
Adam Dia <adambilaldia@protonmail.com>
2025-12-17 15:19:03 -0500

    Firmware basically done

---
+++ firmware/src/main.cpp
@@ -0,0 +1,16 @@
+#include "esp32-hal-adc.h"
+#include <Arduino.h>
+
+const int POT = 4;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    int reading = analogRead(POT);
+    int level = map(reading, 0, 4095, 0, 100);
+    Serial.println(level);
+}
