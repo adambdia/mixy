de9f07aeb4538969dba054e1e4d4157d7e7c961b
Adam Dia <adambilaldia@protonmail.com>
2025-12-18 02:44:19 -0500

    volume control is working

--- host-software/main.py
+++ host-software/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import serial
+import pulsectl
 from time import sleep
 
 
@@ -14,7 +15,12 @@ def volume_from_dial(dial):
         game = 100
         chat = (dial + 50) * 2 - 100
 
-    return game, chat
+    return game / 100, chat / 100
+
+
+def set_volume(pulse, game, chat, game_vol, chat_vol):
+    pulse.volume_set_all_chans(game, game_vol)
+    pulse.volume_set_all_chans(chat, chat_vol)
 
 
 print("Opening serial port...")
@@ -22,17 +28,27 @@ ser = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=1)
 print("Port opened.")
 sleep(1)
 ser.reset_input_buffer()
-with ser:
-    while True:
-        try:
-            data = ser.readline()
-            data = data.decode()
-            data = int(data)
-            game, chat = volume_from_dial(data)
-            print("Data: ", data, "Game: ", game, " Chat: ", chat)
-        except KeyboardInterrupt:
-            print()
-            print("Terminating.")
-            break
-        except:
-            print("Error reading data.")
+
+
+pulse = pulsectl.Pulse("mixy")
+sinks = pulse.sink_list()
+game_sink = next(s for s in sinks if s.name == "input.game_sink")
+chat_sink = next(s for s in sinks if s.name == "input.chat_sink")
+
+while True:
+    try:
+        data = ser.readline()
+        data = data.decode()
+        data = int(data)
+        game_vol, chat_vol = volume_from_dial(data)
+        print("Data: ", data, "Game: ", game_vol, " Chat: ", chat_vol)
+        set_volume(pulse, game_sink, chat_sink, game_vol, chat_vol)
+    except KeyboardInterrupt:
+        break
+    except Exception as e:
+        print(e)
+
+print()
+print("Terminating.")
+ser.close()
+pulse.close()
