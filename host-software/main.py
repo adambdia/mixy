2f873d0518cda00fc53436a965050c8d2bc0faa5
Adam Dia <adambilaldia@protonmail.com>
2025-12-18 00:39:58 -0500

    volume function

--- host-software/main.py
+++ host-software/main.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
+
 import serial
 from time import sleep
+
+
+def volume_from_dial(dial):
+    game = 0
+    chat = 0
+    if dial > 50:
+        chat = 100
+        game = 100 - (dial - 50) * 2
+    else:
+        game = 100
+        chat = (dial + 50) * 2 - 100
+
+    return game, chat
+
 
 print("Opening serial port...")
 ser = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=1)
@@ -9,14 +24,15 @@ sleep(1)
 ser.reset_input_buffer()
 with ser:
     while True:
-        data = ser.readline()
-        data = data.decode()
-        data = data.strip()
-        print(
-            "Data in: ",
-            data,
-            "Game level: ",
-            data,
-            "Chat Level: ",
-            100 - int(data),
-        )
+        try:
+            data = ser.readline()
+            data = data.decode()
+            data = int(data)
+            game, chat = volume_from_dial(data)
+            print("Data: ", data, "Game: ", game, " Chat: ", chat)
+        except KeyboardInterrupt:
+            print()
+            print("Terminating.")
+            break
+        except:
+            print("Error reading data.")
