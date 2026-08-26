bfe63f2acf436fbd1a6a13a93b3ddd01a8c325dc
Adam Dia <adambilaldia@protonmail.com>
2026-08-25 22:05:50 -0400

    added delay to main loop in fw

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,7 +1,10 @@
 #include <Adafruit_TinyUSB.h>
 #include <Arduino.h>
+#include <cstdint>
 
 const int POT = 26;
+const uint32_t LOOP_PERIOD = 100; // 100 ms
+uint32_t last_loop = 0;
 
 void setup()
 {
@@ -13,10 +16,14 @@ void setup()
 
 void loop()
 {
-    int reading = analogRead(POT);
-    int level = map(reading, 0, 4095, 0, 100);
-    char buf[8] = {0};
-    itoa(level, buf, 10);
-    strcat(buf, "\n");
-    Serial.print(buf);
+    if (millis() - last_loop > LOOP_PERIOD)
+    {
+        int reading = analogRead(POT);
+        int level = map(reading, 0, 4095, 0, 100);
+        char buf[8] = {0};
+        itoa(level, buf, 10);
+        strcat(buf, "\n");
+        Serial.print(buf);
+        last_loop = millis();
+    }
 }
